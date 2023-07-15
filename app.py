f9510eac4723a5f1cb273f6ad30d88929d1e2f8d
aitoolsau <aitoolsau@gmail.com>
2023-07-15 17:32:34 +1000

    aider: Added a route in `app.py` to handle form submission from `register.html` and insert new user data into the `users` table in the database.
    # Context:
    USER: I added these *read-write* files: templates/register.html
    ASSISTANT: Great! Now, let's add a route in the `app.py` file to handle the form submission from the `register.html` template. This route will insert the new user data into the `users` table in the `aitools_ebookwriter` database.
    
    ```python
    app.py
    <<<<<<< ORIGINAL
    @app.route('/home')
    def home():
        return render_template('home.html')
    
    if __name__ == '__main__':
        app.run(debug=True)
    =======
    @app.route('/register', methods=['POST'])
    def register_post():
        username = request.form.get('username')
        password = request.form.get('password')
        first_name = request.form.get('first_name')
        second_name = request.form.get('second_name')
        role = request.form.get('role')
        status = request.form.get('status')
    
        cursor = mysql.connection.cursor()
        cursor.execute(''' INSERT INTO users (username, password, first_name, second_name, role, status) VALUES (%s, %s, %s, %s, %s, %s) ''', (username, password, first_name, second_name, role, status))
        mysql.connection.commit()
        cursor.close()
    
        return redirect(url_for('login'))
    
    @app.route('/home')
    def home():
        return render_template('home.html')
    
    if __name__ == '__main__':
        app.run(debug=True)
    >>>>>>> UPDATED
    ```
    
    This new route, `register_post`, will be triggered when the form in the `register.html` template is submitted. It will extract the form data, insert a new row into the `users` table with this data, commit the changes to the database, and then redirect the user to the login page.

--- app.py
+++ app.py
@@ -62,6 +62,22 @@ def register_post():
 
     return redirect(url_for('login'))
 
+@app.route('/register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    first_name = request.form.get('first_name')
+    second_name = request.form.get('second_name')
+    role = request.form.get('role')
+    status = request.form.get('status')
+
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO users (username, password, first_name, second_name, role, status) VALUES (%s, %s, %s, %s, %s, %s) ''', (username, password, first_name, second_name, role, status))
+    mysql.connection.commit()
+    cursor.close()
+
+    return redirect(url_for('login'))
+
 @app.route('/home')
 def home():
     return render_template('home.html')
